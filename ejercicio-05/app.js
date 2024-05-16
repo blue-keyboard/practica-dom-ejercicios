3e28c0540fcd41a24b1a8f2f3e63c3baeb3802eb
blue-keyboard <bluekeyboard9@gmail.com>
2024-05-16 20:03:16 +0200

    add completed exercise 5

---
+++ ejercicio-05/app.js
@@ -0,0 +1,28 @@
+// Basandote en el array siguiente, crea una lista ul > li dinámicamente en el html
+// que imprima cada uno de los albums.
+
+// const albums = [
+//   "De Mysteriis Dom Sathanas",
+//   "Reign of Blood",
+//   "Ride the Lightning",
+//   "Painkiller",
+//   "Iron Fist",
+// ];
+
+const albums = [
+   'De Mysteriis Dom Sathanas',
+   'Reign of Blood',
+   'Ride the Lightning',
+   'Painkiller',
+   'Iron Fist'
+]
+
+const ul = document.createElement('ul')
+
+albums.forEach((album) => {
+   const li = document.createElement('li')
+   li.innerText = album
+   ul.appendChild(li)
+})
+
+document.body.appendChild(ul)
